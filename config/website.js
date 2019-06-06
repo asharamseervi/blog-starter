13291089a55e2d98b7af10f4d58bac0b6c7f3e2d
Asharam <seerviasharam@gmail.com>
2019-06-06 14:12:25 +0530

    Initial Commit: Forked after dark mode 06062019

---
+++ config/website.js
@@ -0,0 +1,28 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'Aaimaa Blog Starter', // Navigation and Site Title
+  siteTitleAlt: 'Lets discuss next genration web technologies.', // Alternative Site title for SEO
+  siteTitleShort: 'Aaimaa Starter Blog', // short_name for manifest
+  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'Aaimaa',
+  author: 'Asharam Seervi', // Author for schemaORGJSONLD
+  organization: 'Aaimaa Web Solutions',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@aaimaawebs', // Twitter Username
+  ogSiteName: 'Aaimaa Gatsby Starter Blog', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitter: 'https://twitter.com/aaimaawebs/',
+  twitterHandle: '@aaimaawebs',
+  github: 'https://github.com/aaimaa/',
+  linkedin: '',
+}
